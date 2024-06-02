2f52716bdf45962a7f973ca8a669e9838a5f9677
uillasnr <wiliasreis@hotmail.com>
2024-06-02 18:56:55 -0300

    feat: add config  styled-Components

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   images: {
-    domains: ['mks-sistemas.nyc3.digitaloceanspaces.com'],
+    domains: ["mks-sistemas.nyc3.digitaloceanspaces.com"],
   },
 };
 

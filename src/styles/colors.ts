e89778a0ba089c9d806d6e423d7a7c2455612161
uillasnr <wiliasreis@hotmail.com>
2024-06-02 17:51:55 -0300

    feat: add Project configuration

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,9 @@
 export const colors = {
+  bg:"#F9F9F9",
   primary: "#FFFFFF",
   secondary: "#0F52BA",
   black: "#000000",
   gray: "#EEEEEE",
-  MediumGray: "#2C2C2C"
+  MediumGray: "#2C2C2C",
+  SilverGray: "#BFBFBF"
 };
